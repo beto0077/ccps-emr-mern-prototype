b310476825454139886034471ec570256d461c87
beto0077 <gilberto.gn.1997@gmail.com>
2023-11-22 02:13:42 -0600

    Remove commented out code and update express.json limit

--- server/index.js
+++ server/index.js
@@ -22,7 +22,6 @@ import PsychologyInfo3Routes from './routes/PsychologyInfo/psychologyInfo3.route
 const app = express();
 
 app.use(cors());
-//app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
