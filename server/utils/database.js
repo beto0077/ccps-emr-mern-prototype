120a20e05d76b9ed2e1ce00ff58fb258fa6a306f
beto0077 <gilberto.gn.1997@gmail.com>
2023-11-22 01:15:21 -0600

    Update database configuration

--- server/utils/database.js
+++ server/utils/database.js
@@ -10,10 +10,17 @@ dotenv.config();
     password: process.env.GOOGLE_CLOUD_PSWD,
     database: 'ccps_db'
 })*/
-export const database = createPool({
+/*export const database = createPool({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'Msqlgn220723-',
     database: 'ccps_db'
+})*/
+export const database = createPool({
+    host: process.env.FMSH_HOST,
+    port: process.env.FMSH_PORT,
+    user: process.env.FMSH_USER,
+    password: process.env.FMSH_PASSWORD,
+    database: process.env.FMSH_DATABASE
 })
